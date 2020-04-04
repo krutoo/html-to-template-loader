7cf75c49c9046fa27eaecc2124d0e8ab490b17c5
Petrov Dmitry <dima.krutoo@yandex.ru>
2020-04-04 11:31:49 +0500

    demo webpack config updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,38 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// demo build
+// example build
 module.exports = {
-	mode: 'development',
-	entry: {
-		'top-banner': './src/top-banner.js',
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-      			exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-					},
-				},
-			},
-		],
-	},
+  mode: 'development',
+  entry: './example/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'example/dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+
+      // import html files as strings ready to pass in <template>
+      {
+        test: /\.html$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: path.resolve(__dirname, 'index.js'), // index.js aka 'html-to-template-loader'
+        },
+      },
+    ],
+  },
+
+  // create demo page
+  plugins: [new HtmlWebpackPlugin()],
 };
